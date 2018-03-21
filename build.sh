e9e8ce67769921ad5192084c1e75f6206efa5d7c
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2018-03-21 12:09:09 +0800

    build.sh added - not yet tested

---
+++ build.sh
@@ -0,0 +1,66 @@
+set -ex
+
+yum update -y
+yum install -y \
+    atlas-devel \
+    atlas-sse3-devel \
+    blas-devel \
+    gcc \
+    gcc-c++ \
+    lapack-devel \
+    python27-devel \
+    python27-virtualenv \
+    findutils \
+    xz \
+    zip
+
+rm -rf /var/runtime /var/lang && \
+  curl https://lambci.s3.amazonaws.com/fs/python3.6.tgz | tar -xz -C / && \
+  curl https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz | tar -xJ && \
+  cd Python-3.6.1 && \
+  ./configure --prefix=/var/lang && \
+  make -j$(getconf _NPROCESSORS_ONLN) libinstall inclinstall && \
+  cd .. && \
+  rm -rf Python-3.6.1
+
+do_pip () {
+    #pip install --upgrade pip wheel
+    /var/lang/bin/pip3 install --use-wheel --no-binary numpy numpy
+    /var/lang/bin/pip3 install --use-wheel --no-binary scipy scipy
+    /var/lang/bin/pip3 install --use-wheel sklearn
+}
+
+strip_virtualenv () {
+    echo "venv original size $(du -sh $VIRTUAL_ENV | cut -f1)"
+    find $VIRTUAL_ENV/lib64/python3.6/site-packages/ -name "*.so" | xargs strip
+    echo "venv stripped size $(du -sh $VIRTUAL_ENV | cut -f1)"
+
+    pushd $VIRTUAL_ENV/lib64/python3.6/site-packages/ && zip -r -9 -q /outputs/venv.zip * ; popd
+    echo "site-packages compressed size $(du -sh /outputs/venv.zip | cut -f1)"
+
+    pushd $VIRTUAL_ENV && zip -r -q /outputs/full-venv.zip * ; popd
+    echo "venv compressed size $(du -sh /outputs/full-venv.zip | cut -f1)"
+}
+
+shared_libs () {
+    libdir="$VIRTUAL_ENV/lib64/python3.6/site-packages/lib/"
+    mkdir -p $VIRTUAL_ENV/lib64/python3.6/site-packages/lib || true
+    cp /usr/lib64/atlas/* $libdir
+    cp /usr/lib64/libquadmath.so.0 $libdir
+    cp /usr/lib64/libgfortran.so.3 $libdir
+}
+
+main () {
+    /usr/bin/virtualenv \
+        --python /var/lang/bin/python3 /sklearn_build \
+        --always-copy \
+        --no-site-packages
+    source /sklearn_build/bin/activate
+
+    do_pip
+
+    shared_libs
+
+    strip_virtualenv
+}
+main
